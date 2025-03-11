5aeb0df4b2e2de9f7fbc9f10bba9620da26edc87
maksimkir <kiriluk157@gmail.com>
2025-03-11 20:49:09 +0200

    add class employee

--- main.cpp
+++ main.cpp
@@ -28,81 +28,13 @@ public:
     virtual void showInfo() const {
         cout << "ID: " << id << "\nSalary: " << salary << endl;
     }
-
     virtual double calculate() const {
         return salary;
     }
-
     virtual ~Employee() {}
-
     // Перевантаження оператора << (дружня функція)
     friend ostream& operator<<(ostream& os, const Employee& emp) {
         os << "Employee ID: " << emp.id << "\nSalary: " << emp.salary;
         return os;
     }
 };
-
-class FulltimeEmployee : public Employee {
-private:
-    double bonus;
-public:
-    FulltimeEmployee(int id, double salary, double bonus) : Employee(id, salary), bonus(bonus) {}
-
-    void showInfo() const override {
-        cout << "Full-Time Employee: " << id << "\nSalary: " << salary << "\nBonus: " << bonus << endl;
-    }
-
-    double calculate() const override {
-        return salary + bonus;
-    }
-};
-
-class ParttimeEmployee : public Employee {
-private:
-    int hoursWorked;
-    double hourlyRate;
-public:
-    ParttimeEmployee(int id, int hours, double rate) : Employee(id, 0), hoursWorked(hours), hourlyRate(rate) {}
-
-    void showInfo() const override {
-        cout << "Part-Time Employee ID: " << id << "\nHours Worked: " << hoursWorked << "\nHourly Rate: " << hourlyRate << endl;
-    }
-
-    double calculate() const override {
-        return hoursWorked * hourlyRate;
-    }
-};
-
-class PayrollManager {
-private:
-    vector<Employee*> employees;
-
-public:
-    void addEmployee(Employee* e) {
-        employees.push_back(e);
-    }
-
-    void processPayroll() const {
-        for (const auto& employee : employees) {
-            employee->showInfo();
-            cout << "Total Pay: " << employee->calculate() << "\n";
-        }
-    }
-
-    ~PayrollManager() {
-        for (auto& e : employees) {
-            delete e;
-        }
-    }
-};
-
-int main() {
-    PayrollManager manager;
-    manager.addEmployee(new FulltimeEmployee(1, 3000, 500));
-    manager.addEmployee(new ParttimeEmployee(2, 60, 20));
-
-    cout << "Payroll Processing: " << endl;
-    manager.processPayroll();
-
-    return 0;
-}
